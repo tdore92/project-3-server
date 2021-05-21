3e3685fc29c2b16fce1036d48e06b3afd0b3620d
Drew Lamph <drewlamph@gmail.com>
2021-05-21 16:45:51 +0100

    adding activity model

--- db/data/activities.js
+++ db/data/activities.js
@@ -5,7 +5,7 @@ export default [
     activity: 'Hike',
     season: 'Summer',
     name: 'Mount Snowdon Hike',
-    image: 'fake url',
+    image_url: 'fake url',
     description: 'Mount Snowdon Via Miner\'s Track is a 8.1 mile out and back trail located near Llanberis, Gwynedd, Wales that features a lake and is rated as difficult. The trail is primarily used for hiking, camping, nature trips, and backpacking and is accessible year-round.',    
   },
   { 
@@ -14,7 +14,8 @@ export default [
     activity: 'Snow',
     season: 'Winter',
     name: 'Grands Montets, Chamonix',
-    description: 'The most iconic ski area for advanced skiers and riders has to be the Grands Montets sector. Here you can revel in a vast freeride heaven and tackle the 2,000m descent down to the valley when you’re done. Skiing the famous Vallée Blanche from the top of the Aiguille du Midi cable car with a qualified guide is also a must for anybody looking to test their skills.'
+    image_url: 'fake url',
+    description: 'The most iconic ski area for advanced skiers and riders has to be the Grands Montets sector. Here you can revel in a vast freeride heaven and tackle the 2,000m descent down to the valley when you’re done. Skiing the famous Vallée Blanche from the top of the Aiguille du Midi cable car with a qualified guide is also a must for anybody looking to test their skills.',
   },
   {
     number: 3,
@@ -22,7 +23,8 @@ export default [
     activity: 'Hike',
     season: 'Summer',
     name: 'Gratweg Stoos',
-    description: 'The two Mythen peaks to the right; to the left, first the Riemenstalden valley, then Lake Uri – a succession of sweeping vistas! Chairlift ascent to the Klingenstock and scenic hike to Fronalpstock with panoramic ridge views as well as extensive views from the mountain house. Valley descent to Stoos again via chairlift.'
+    image_url: 'fake url',
+    description: 'The two Mythen peaks to the right; to the left, first the Riemenstalden valley, then Lake Uri – a succession of sweeping vistas! Chairlift ascent to the Klingenstock and scenic hike to Fronalpstock with panoramic ridge views as well as extensive views from the mountain house. Valley descent to Stoos again via chairlift.',
   },
   {
     number: 4,
@@ -30,7 +32,8 @@ export default [
     activity: 'Snow',
     season: 'Winter',
     name: 'Saint Gotthard Pass',
-    description: 'A route in use since the 13th century, it is one of the main roads through the Alps with the Mont Blanc tunnel to the west and the Brenner Pass further to the east.'
+    image_url: 'fake url',
+    description: 'A route in use since the 13th century, it is one of the main roads through the Alps with the Mont Blanc tunnel to the west and the Brenner Pass further to the east.',
   },
   {
     number: 5,
@@ -38,6 +41,7 @@ export default [
     activity: 'Snow',
     season: 'Winter',
     name: 'San Cassiano',
-    description: 'The awe-inspiring peaks of the dramatically jagged Dolomites are unlike anywhere else you’ll have seen. With breathtaking scenery, some incredible ski terrain and plenty of pretty mountain villages, there are so many reasons to visit the Dolomites. And that’s without mentioning the mouth-watering cuisine and hospitality that’s second to none. You can’t get much closer to ski paradise.'
+    image_url: 'fake url',
+    description: 'The awe-inspiring peaks of the dramatically jagged Dolomites are unlike anywhere else you’ll have seen. With breathtaking scenery, some incredible ski terrain and plenty of pretty mountain villages, there are so many reasons to visit the Dolomites. And that’s without mentioning the mouth-watering cuisine and hospitality that’s second to none. You can’t get much closer to ski paradise.',
   }
 ]
