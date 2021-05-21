fb054214869a5ce3e36ca24648ef159f1848b0f3
Drew Lamph <drewlamph@gmail.com>
2021-05-21 15:48:19 +0100

    database add

---
+++ db/data/activities.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    number: 1,
+    country: 'United Kingdom',
+    activity: 'Hike',
+    season: 'Summer',
+    name: 'Mount Snowdon Hike',
+    image: 'fake url',
+    description: 'Mount Snowdon Via Miner\'s Track is a 8.1 mile out and back trail located near Llanberis, Gwynedd, Wales that features a lake and is rated as difficult. The trail is primarily used for hiking, camping, nature trips, and backpacking and is accessible year-round.',    
+  },
+  { 
+    number: 2,
+    country: 'France',
+    activity: 'Snow',
+    season: 'Winter',
+    name: 'Grands Montets, Chamonix',
+    description: 'The most iconic ski area for advanced skiers and riders has to be the Grands Montets sector. Here you can revel in a vast freeride heaven and tackle the 2,000m descent down to the valley when you’re done. Skiing the famous Vallée Blanche from the top of the Aiguille du Midi cable car with a qualified guide is also a must for anybody looking to test their skills.'
+  }
+  {
+    number: 3,
+    country: 'Switzerland',
+    activity: 'Hike',
+    season: 'Summer',
+    name: 'Gratweg Stoos',
+    description: 'The two Mythen peaks to the right; to the left, first the Riemenstalden valley, then Lake Uri – a succession of sweeping vistas! Chairlift ascent to the Klingenstock and scenic hike to Fronalpstock with panoramic ridge views as well as extensive views from the mountain house. Valley descent to Stoos again via chairlift.'
+  },
+  {
+    number: 4,
+    country: 'Switzerland',
+    activity: 'Snow',
+    season: 'Winter',
+    name: 'Saint Gotthard Pass',
+    description: 'A route in use since the 13th century, it is one of the main roads through the Alps with the Mont Blanc tunnel to the west and the Brenner Pass further to the east.'
+  }
+  {
+    number: 5,
+    country: 'Italy',
+    activity: 'Snow',
+    season: 'Winter',
+    name: 'San Cassiano',
+    description: 'The awe-inspiring peaks of the dramatically jagged Dolomites are unlike anywhere else you’ll have seen. With breathtaking scenery, some incredible ski terrain and plenty of pretty mountain villages, there are so many reasons to visit the Dolomites. And that’s without mentioning the mouth-watering cuisine and hospitality that’s second to none. You can’t get much closer to ski paradise.'
+  }
+]
