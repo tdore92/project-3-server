efbe5e0c1fe3d0044a75d50e53371a9f6a168c10
Theo Wyzgowski <theowyz@btinternet.com>
2021-05-24 16:12:19 +0100

    embedding

---
+++ controllers/comment.js
@@ -0,0 +1,58 @@
+import Activity from '../models/activities.js'
+import { NotFound } from '../lib/errors.js'
+
+
+async function create(req, res, next) {
+  req.body.user = req.currentUser
+  try {
+    const activity = await Activity.findById(req.params.id)
+      .populate('user')
+      .populate('comments.user')
+
+    if (!activity) {
+      throw new NotFound('No activity found.')
+    }
+    activity.comments.push(req.body)
+
+    const savedActivity = await activity.save()
+    res.send(savedActivity)
+  } catch (e) {
+    next(e)
+  }
+}
+
+
+
+
+async function update(req, res, next) {
+  try {
+    const { activityId, commentId } = req.params
+
+    const activity = await Activity.findById(activityId)
+
+    if (!activity) {
+      throw new NotFound('No activity found.')
+    }
+
+    const comment = activity.comments.id(commentId)
+
+    if (!req.currentUser._id.equals(comment.user)) {
+      return res.status(401).send({ message: 'Unauthorized' })
+    }
+
+    comment.set(req.body)
+    const savedActivity = await activity.save()
+
+    res.send(savedActivity)
+
+  } catch (e) {
+    next(e)
+  }
+
+}
+
+
+export default {
+  create,
+  update,
+}
--- models/activities.js
+++ models/activities.js
@@ -1,6 +1,14 @@
 import mongoose from 'mongoose'
 
-const activitySchema = mongoose.Schema({
+//embedded schema
+const commentSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+}, {
+  timestamps: true,
+})
+
+const activitySchema = new mongoose.Schema({
   number: { type: Number, required: true },
   country: { type: String, required: true },
   activity: { type: String, required: true },
@@ -8,6 +16,9 @@ const activitySchema = mongoose.Schema({
   season: { type: String, required: true },
   image_url: { type: String, required: true },
   description: { type: String, required: true },
+
+  user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+  comments: [commentSchema],
 })
 
 export default mongoose.model('Activities', activitySchema)
