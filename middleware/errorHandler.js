82eaaffd24d970de2c510824fbff5ba2a6679621
Drew Lamph <drewlamph@gmail.com>
2021-05-24 12:23:41 +0100

    server updated

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -3,10 +3,10 @@ export default function errorHandler(err, req, res, next) {
   console.log(err.name)
   console.log(err)
 
-  if (err.name === 'Cast Error') {
+  if (err.name === 'CastError') {
     return res.status(400).json({ message: 'Invalid parameter given' })
   }
-  if (err.name === 'Not Found') {
+  if (err.name === 'NotFound') {
     return res.status(404).json({ message: 'Not Found' })
   }
 
