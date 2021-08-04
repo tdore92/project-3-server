39d6661f40bb0f83b78f60e056864d12c5e09e24
Theo Wyzgowski <theowyz@btinternet.com>
2021-08-04 18:35:21 +0100

    server deployment take 5

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const dbURI =
+export const dbURL =
   process.env.DB_URI || 'mongodb://localhost/activitiesdb'
 export const port = process.env.PORT || 4000
 export const secret = process.env.SECRET || 'shhhh its a secret'
